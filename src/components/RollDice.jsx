219a777e595116652186ae81e4e0633ef41d6bda
Abir Chodha <abirchodha1@gmail.com>
2025-01-13 19:37:11 +0530

    Create Roll Dice component

---
+++ src/components/RollDice.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const RollDice = () => {
+  const [currentDice, setCurrentDice] = useState(1);
+
+  const generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  const rollDice = () => {
+    setCurrentDice(generateRandomNumber(1, 7));
+  };
+
+  return (
+    <DiceContainer>
+      <div className="dice" onClick={rollDice}>
+        <img
+          src={`/images/dice/dice_${currentDice}.png`}
+          alt={`Dice ${currentDice}`}
+        />
+      </div>
+      <p>Click on Dice to Roll</p>
+    </DiceContainer>
+  );
+};
+
+export default RollDice;
+
+const DiceContainer = styled.div`
+  margin-top: 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .dice {
+    cursor: pointer;
+  }
+`;
