3641b86e0b172585fb87ff4a0343f23b7b765b80
Abir Chodha <abirchodha1@gmail.com>
2025-01-13 19:03:42 +0530

    Place totalScore and NumberSelector in gameplay

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+import NumberSelector from "./NumberSelector";
+import TotalScore from "./TotalScore";
+
 const GamePlay = () => {
-  return <div>GamePlay</div>;
+  return (
+    <MainContainer>
+      <div className="top_section">
+        <TotalScore />
+        <NumberSelector />
+      </div>
+    </MainContainer>
+  );
 };
 
 export default GamePlay;
+
+const MainContainer = styled.main`
+  padding-top: 70px;
+
+  .top_section {
+    display: flex;
+    justify-content: space-around;
+    align-items: end;
+  }
+`;
--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -6,21 +6,40 @@ const NumberSelector = () => {
   const [selectedNumber, setSelectedNumber] = useState();
 
   return (
-    <div>
-      {arrNumber.map((value, i) => (
-        <Box
-          isSelected={value === selectedNumber}
-          key={i}
-          onClick={() => setSelectedNumber(value)}
-        >
-          {value}
-        </Box>
-      ))}
-    </div>
+    <NumberSelectorContainer>
+      <div className="flex">
+        {arrNumber.map((value, i) => (
+          <Box
+            isSelected={value === selectedNumber}
+            key={i}
+            onClick={() => setSelectedNumber(value)}
+          >
+            {value}
+          </Box>
+        ))}
+      </div>
+      <p>Select Number</p>
+    </NumberSelectorContainer>
   );
 };
 
 export default NumberSelector;
+
+const NumberSelectorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+
+  .flex {
+    display: flex;
+    gap: 24px;
+  }
+
+  p {
+    font-size: 24px;
+    font-weight: 700px;
+  }
+`;
 
 const Box = styled.div`
   height: 72px;
